f6e6296061fc2a2af8a8495306be888ba9134e75
Mikko Piuhola <mikko.piuhola@gmail.com>
2016-12-15 23:36:09 +0200

    06 - only load data once, add highlighting

--- 06 - Ajax Type Ahead/main.js
+++ 06 - Ajax Type Ahead/main.js
@@ -4,35 +4,54 @@
     const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
     const inputEl = document.getElementById('location-search');
     const suggestionsEl = document.getElementById('suggestions');
+    const fullData = [];
+    let filteredData = [];
+
+    function init() {
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(data => data.map(d => {
+                d.population = parseInt(d.population, 10);
+                return d;
+            }))
+            .then(data => fullData.push(...data))
+            .catch(err => console.error(err));
+
+        inputEl.addEventListener('input', onInput);
+    }
 
     function onInput({ currentTarget }) {
         const input = currentTarget.value;
 
         if (!input) {
+            showResults(fullData, null);
             return false;
         }
 
-        fetch(endpoint)
-            .then(res => res.json())
-            .then(data => {
-                data = data.filter(item => item.city.indexOf(input) > -1 || item.state.indexOf(input) > -1);
-                return showResults(data)
-            })
-            .catch(err => {
-                console.error(err);
-            });
+        filteredData = fullData.filter(item => {
+            const regex = new RegExp(input, 'gi');
+            return item.city.match(regex) || item.state.match(regex);
+        });
+        showResults(filteredData, input);
     }
 
-    function showResults(results) {
+    function showResults(results, input) {
+        // TODO: Chunked render (this is way too heavy)
         suggestionsEl.innerHTML = results
             .map(result => {
+                const highlightRegex = new RegExp(input, 'gi');
+                const city = result.city.replace(highlightRegex, `<span class="hl">${input}</span>`);
+                const state = result.state.replace(highlightRegex, `<span class="hl">${input}</span>`);
+
+
                 return `<li class="suggestion-item">
-                    ${result.city}, ${result.state}
+                    <span class="name">${city}, ${state}</span>
+                    <span class="population">${result.population.toLocaleString('fi-fi')}</span>
                 </li>`
             })
             .join('');
     }
 
-    inputEl.addEventListener('input', onInput);
+    init();
     
 })();
